a8b462f7a72cb655e3872f75ffbd535c00c5d742
bigtig <tjrgh25@gmail.com>
2025-01-15 16:08:46 +0900

    week3: exercise modified

---
+++ BIG_semanticKernel/week3/LightModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace week3
+{
+    public class LightModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("is_on")]
+        public bool? IsOn { get; set; }
+
+        [JsonPropertyName("brightness")]
+        public byte? Brightness { get; set; }
+
+        [JsonPropertyName("hex")]
+        public string? Hex { get; set; }
+    }
+
+
+}
--- BIG_semanticKernel/week3/obj/Debug/net8.0/week3.AssemblyInfo.cs
+++ BIG_semanticKernel/week3/obj/Debug/net8.0/week3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("week3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64374d30caf1963a4f90ea2ffc792ff72e371e40")]
 [assembly: System.Reflection.AssemblyProductAttribute("week3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("week3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
